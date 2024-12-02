69ca49c95b23eafebd090171c64e9829f15d47d4
IanStroemkjaerJensen <IanStroemkjaerJensen@gmail.com>
2024-12-02 19:09:10 +0100

    Some cleanup and comment to keep checks to a maximum of 40 to stay within rate limits

--- main.py
+++ main.py
@@ -4,22 +4,21 @@ import time
 
 
 # Input text
-list_of_names = """
+list_of_potential_domains = """
+
 """
 
-# Regex to remove spaces, input quotation marks around and finish the word of with a comma for
-# every string in the array
-result1 = re.sub(r'(?m)^(\w+)\s+(\w+)', r"'\1\2',", list_of_names)
+list_of_testing_names = """
 
-# Regex with prefix(here "AI", to remove space between two words, input quotation marks around and finish the word of with a comma for
-# on everything coming after the prefix "AI"
-result2 = re.sub(r'(?m)^AI\s+(\w+)(\w*)', r"'AI\1\2',", list_of_names)
+"""
 
-#print(result1)
+# Step 1: Regex to add quotes around each word, remove spaces, and add a comma at the end
+result1 = re.sub(r'(?m)^\s*(\w+)\s*(\w+)', r"'\1\2',", list_of_testing_names)
 
 
-# Generate domain names from the formatted result adding the .com suffix
-domain_names_with_dotcom = [item.strip("' ,") + ".com" for item in result1.splitlines()]
+# Step 2: Generate domain names by appending .com to each line
+domain_names_with_dotcom = [item.strip(", '") + ".com" for item in result1.splitlines()]
+
 
 # Print the formatted result
 print(domain_names_with_dotcom)
@@ -36,18 +35,18 @@ def check_domain_availability(domain_name):
         domain_info = whois.whois(domain_name)
         # If the WHOIS response contains information, it's registered
         if domain_info.status is not None:
-            print(f" '{domain_name}'is already registered")
+            print(f" '{domain_name}' is already registered")
             return False
     except Exception:
         # WHOIS library raises an exception if the domain is not registered
         print(f"The domain '{domain_name}' is available!")
         return True
 
-# keep the list to a maximum of 75 at a time
+# keep the list to a maximum of 40 at a time
 # Test the function
 if __name__ == "__main__":
     for domain in domain_names_with_dotcom:
         check_domain_availability(domain)
         time.sleep(3) # Waits 3 seconds between lookups to stay within rate limit and not overwork Registrar servers
-    print("done")
+    print("Done")
 
